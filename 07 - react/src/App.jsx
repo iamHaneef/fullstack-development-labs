6ca6587bc586ce5a19483bb249b3a57c75a55a06
iamHaneef <muhamadhaneef.official@gmail.com>
2026-05-04 16:51:59 +0530

    Update the Readme Doc

--- 07 - react/src/App.jsx
+++ 07 - react/src/App.jsx
@@ -1,11 +1,12 @@
 import ArrayCal from "./17 - UseMemo ( Hooks  )/ArrayCal"
 import FilteredUser from "./17 - UseMemo ( Hooks  )/FilteredUser"
+import FilteredProduct from "./17 - UseMemo ( Hooks  )/FilteredProduct"
 
 function App() {
 
   return (
     <>
-      <FilteredUser />
+      <FilteredProduct />
     </>
   )
 }
