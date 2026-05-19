1c87142c3ab3e7e6d03291632a5d17cc7dc420af
iamHaneef <muhamadhaneef.official@gmail.com>
2026-05-19 15:23:50 +0530

    Practise Reducer and Redux

--- 07 - react/src/App.jsx
+++ 07 - react/src/App.jsx
@@ -3,13 +3,14 @@ import Products from "./18 - Callback Memoization (useCallback)/Products"
 import CounterReducer from "./19 - Reducer (useReducer)/CounterReducer"
 import TodoApp from "./19 - Reducer (useReducer)/TodoApp"
 import Counter from "./20 - Redux Toolkit/01 - Redux Thinking/Counter"
+import Todo from "./20 - Redux Toolkit/02 - Todo Reducer App/Todo"
 
 
 function App() {
 
   return (
     <>
-      <Counter />
+      <Todo />
     </>
   )
 }
