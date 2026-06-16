6f58cf3dd3d69a6edb1bdae95c110edd055cac11
iamHaneef <muhamadhaneef.official@gmail.com>
2026-06-16 15:05:03 +0530

    Refactor Footer and DigitalClock styles for improved formatting and consistency

--- 07 - react/src/01 - Basics/Footer.jsx
+++ 07 - react/src/01 - Basics/Footer.jsx
@@ -1,10 +1,9 @@
 function Footer() {
-    return (
-        <>
-            
-            <h2> &copy; 2023 Haneef Fruit Shop Websiite</h2>
-        </>
-    )
+  return (
+    <>
+      <h2> &copy; 2023 Haneef Fruit Shop Websiite</h2>
+    </>
+  );
 }
 
 export default Footer;
